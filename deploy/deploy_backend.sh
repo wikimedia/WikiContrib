d913c3398f62003fb93a8143e085dce2719e2aed
rammanoj <rammanojpotla1608@gmail.com>
2019-11-29 19:02:57 +0530

    Add Continous Integration and testing.

---
+++ deploy/deploy_backend.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+ssh -o "StrictHostKeyChecking=no" -i /tmp/deploy_rsa -A rampotla@login.tools.wmflabs.org << EOT
+become contraband
+source ~/www/python/venv/bin/activate
+echo "Activated the environment\n"
+cd ~/www/python/
+git clone https://github.com/wikimedia/WikiContrib/
+echo "Cloned the repository\n"
+mv src/contraband/.env WikiContrib/backend/Contraband/contraband/.env
+cd src/
+rm -rf contraband/ Install.md manage.py query/ result/ test_data/ requirements.txt db.sqlite3
+cp -r ../WikiContrib/backend/Contraband/* .
+cd contraband
+mv local_settings.py local_settings_sample.py
+cd ../
+echo "Performed the necessary changes\n"
+python manage.py makemigrations
+python manage.py migrate
+echo "Ran updated migrations\n"
+webservice --backend=kubernetes python3.5 restart
+cd ../
+rm -rf WikiContrib src/contraband/.env.example
+deactivate
+exit
+exit
---
+++ deploy/deploy_frontend.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+ssh -o "StrictHostKeyChecking=no" -i /tmp/deploy_rsa -A rampotla@login.tools.wmflabs.org << EOT
+become WikiContrib
+rm -rf ~/public_html/
+mkdir ~/public_html/
+cd ~/public_html/
+git clone https://github.com/wikimedia/WikiContrib/
+cd WikiContrib/frontend/WikiContrib-Frontend/
+npm run build
+cp -a ./build/. ../../../
+cd ../../../
+rm -rf WikiContrib
