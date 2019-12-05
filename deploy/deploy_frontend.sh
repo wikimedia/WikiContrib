bfd80fe780b15baa555a434fcfd0fe3eb8892756
rammanoj <rammanojpotla1608@gmail.com>
2019-12-05 17:32:06 +0530

    Test travis

--- deploy/deploy_frontend.sh
+++ deploy/deploy_frontend.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
-ssh -o "StrictHostKeyChecking=no" -i /tmp/deploy_rsa -A rampotla@login.tools.wmflabs.org << EOT
-become WikiContrib
+ssh -o "StrictHostKeyChecking=no" -i /tmp/deploy_rsa -A ${DEPLOY_USERNAME}@${DEPLOY_HOST} << EOT
+become ${DEPLOY_FRONTEND}
 rm -rf ~/public_html/
-mkdir ~/public_html/
-cd ~/public_html/
-git clone https://github.com/wikimedia/WikiContrib/
-cd WikiContrib/frontend/WikiContrib-Frontend/
+git clone https://github.com/wikimedia/WikiContrib
+echo "Cloned the repo"
+cp -r WikiContrib/frontend/WikiContrib-Frontend/. dep/
+rm -rf WikiContrib/
+cd dep/
+mv dist/ src/dist/
 npm run build
-cp -a ./build/. ../../../
-cd ../../../
-rm -rf WikiContrib
+echo "Creating the build folder"
+mv src/dist/ dist/
+cd ../
+mkdir public_html/
+cp -r dep/build/. public_html/
+cd dep/
+rm -rf build/ package.json package-lock.json public/ README.md src/ Install.md
+echo "Performed all the shell operations."
+webservice lighttpd restart
+EOT
