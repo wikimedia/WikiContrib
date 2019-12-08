842a1d06f011036cce7f3a81491e3109785ced62
rammanoj <rammanojpotla1608@gmail.com>
2019-12-08 12:28:34 +0530

    Delete pre_deploy_frontend.sh

--- deploy/pre_deploy_frontend.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd frontend/WikiContrib-Frontend/
-npm run build
-cd build
-git init
-git add .
-git commit -m "Deploy commit"
-git push -f https://${GIT_USERNAME}:${GIT_PASS}@${GIT_REPO} --all
-echo "Pushed the Repo for deployment"
