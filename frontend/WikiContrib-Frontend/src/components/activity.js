5dcf7016310eeea30f1e4d48169b60577be3498b
Juan <39202665+ajuancer@users.noreply.github.com>
2019-12-18 16:48:54 +0100

    Implementing platform and status icons.
    For more information, visit the issue #71 of wikimedia/WikiContrib

--- frontend/WikiContrib-Frontend/src/components/activity.js
+++ frontend/WikiContrib-Frontend/src/components/activity.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { fetchAsynchronous } from './fetch';
 import { commits_by_date } from './../api';
-import { Card, Placeholder, Header } from 'semantic-ui-react';
-
+import { Card, Placeholder, Header, Button, Popup } from 'semantic-ui-react';
+import gerritPlatformIcon from '../img/gerritPlatformIcon.png';
+import phabricatorPlatformIcon from '../img/phabricatorPlatformIcon.png';
+import abandonedStatusIcon from '../img/abandonedStatusIcon.png';
+import openStatusIcon from '../img/openStatusIcon.png';
+import mergedStatusIcon from '../img/mergedStatusIcon.png';
+import resolvedStatusIcon from '../img/resolvedStatusIcon.png';
+import invalidStatusIcon from '../img/invalidStatusIcon.png';
+import stalledStatusIcon from '../img/stalledStatusIcon.png';
+import rejectedStatusIcon from '../img/rejectedStatusIcon.png';
 /**
  * Show all the user Commits on a specific day
  */
@@ -48,6 +56,52 @@ class Activity extends React.Component {
     });
   };
 
+  /**
+   * Select corresponding platform and status icon.
+   * @param {string} platform The platform of the activity.
+   * @param {string} status The status of the contribution.
+   * @return {Array} The paths of the corresponding platform icon and status.
+   */
+  cardIconsPaths = (platform, status) => {
+    let platformPath;
+    let statusPath;
+    if (platform.toLowerCase() == "gerrit") {
+      platformPath = gerritPlatformIcon;
+      if (status.toLowerCase() == "abandoned") {
+        statusPath = abandonedStatusIcon;
+      } else if (status.toLowerCase() == "open") {
+        statusPath = openStatusIcon;
+      } else {
+        statusPath = mergedStatusIcon;
+      }
+    } else {
+      platformPath = phabricatorPlatformIcon;
+      if (status.toLowerCase() == "resolved") {
+        statusPath = resolvedStatusIcon;
+      } else if (status.toLowerCase() == "invalid") {
+        statusPath = invalidStatusIcon;
+      } else if (status.toLowerCase() == "stalled") {
+        statusPath = stalledStatusIcon;
+      } else {
+        statusPath = rejectedStatusIcon;
+      }
+    }
+    return {
+      platform: platformPath,
+      status: statusPath
+    };
+  };
+
+  /**
+   * Stylize given word, capitalizing the first letter.
+   * @param {string} originalWord The word you want to normalize.
+   * @return {string} The same word with first letter capitalized.
+   */
+  normalizeWord = (originalWord) => {
+    originalWord = originalWord.toLowerCase();
+    return originalWord.slice(0,1).toUpperCase() + originalWord.slice(1);
+  };
+
   render = () => {
     return (
       <React.Fragment>
@@ -72,39 +126,45 @@ class Activity extends React.Component {
           <React.Fragment>
             {this.state.data.length !== 0 ? (
               <React.Fragment>
-                <h4>User Activity on {this.props.date}</h4>
+                <h4>{this.props.username}'s activity on {this.props.date}</h4>
                 {this.state.data.map((obj, index) => (
                   <Card className="commits_load" key={index}>
                     <Card.Content>
-                      {obj.platform === 'Phabricator' ? (
-                        <a
-                          href={
-                            'https://phabricator.wikimedia.org/' + obj.redirect
+                    <div class="card">
+                      <div class="platform">
+                        <Popup
+                          content={this.normalizeWord(obj.platform)}
+                          position="top center"
+                          trigger={
+                            <a href={obj.platform.toLowerCase() == "phabricator" ? ("https://phabricator.wikimedia.org/"
+                          ) : (
+                            "https://gerrit.wikimedia.org/r/#/q/"
+                          )
+                        }><img height="40px" src={this.cardIconsPaths(obj.platform, obj.status).platform}></img></a>
                           }
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
+                        />
+                      </div>
+                      <div class="title">
+                      {obj.platform === 'Phabricator' ? (
+                        <a href={'https://phabricator.wikimedia.org/' + obj.redirect} target="_blank" rel="noopener noreferrer">
                           <h3>{obj.heading}</h3>
                         </a>
                       ) : (
-                        <a
-                          href={
-                            'https://gerrit.wikimedia.org/r/#/q/' + obj.redirect
-                          }
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
+                        <a href={'https://gerrit.wikimedia.org/r/#/q/' + obj.redirect} target="_blank" rel="noopener noreferrer">
                           <h3>{obj.heading}</h3>
                         </a>
                       )}
-                      <div>
-                        <span style={{ display: 'inline', float: 'left' }}>
-                          <b>PLATFORM:</b> {obj.platform}
-                        </span>
-                        <span style={{ display: 'inline', float: 'right' }}>
-                          <b>STATUS:</b> {obj.status}
-                        </span>
                       </div>
+                      <div class="status">
+                        <Popup
+                          content={this.normalizeWord(obj.status)}
+                          position="top center"
+                          trigger={
+                            <img height="40px" src={this.cardIconsPaths(obj.platform, obj.status).status}></img>
+                          }
+                          />
+                      </div>
+                    </div>
                     </Card.Content>
                   </Card>
                 ))}
