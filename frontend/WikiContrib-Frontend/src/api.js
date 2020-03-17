59cfe68bfd65214bef61f3b50274fef82c64513d
Yael Luria <yaeluria@gmail.com>
2020-03-17 20:33:58 +0200

    remove comment

--- frontend/WikiContrib-Frontend/src/api.js
+++ frontend/WikiContrib-Frontend/src/api.js
@@ -10,7 +10,7 @@ export const QueryCreateApi = BASE_API_URI + 'query/add/user/';
 // method: GET, PATCH, DELETE
 export const QueryDetailApi = BASE_API_URI + 'query/<hash>/update/user/';
 // method: POST
-export const filterCreateApi = BASE_API_URI + 'query/<hash>/add/filter/'; //where is this used?
+export const filterCreateApi = BASE_API_URI + 'query/<hash>/add/filter/'; 
 // method: GET, PATCH, DELETE
 export const filterDetailApi = BASE_API_URI + 'query/<hash>/update/filter/';
 // method: POST
