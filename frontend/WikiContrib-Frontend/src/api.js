9d79bd8185bfe8fe2f277594b6f8a3f1b11c3889
Raymond Ndibe <ndiberaymond1@gmail.com>
2020-03-24 23:04:48 +0100

    automated backend url selection based on whether development or production evironment is running

--- frontend/WikiContrib-Frontend/src/api.js
+++ frontend/WikiContrib-Frontend/src/api.js
@@ -2,15 +2,16 @@
  All the Global Constants are declared here.
 */
 
-const BASE_API_URI = 'https://tools.wmflabs.org/contraband/';
-// const BASE_API_URI = 'http://127.0.0.1:8000/';
+const BASE_API_URI = process.env.NODE_ENV === 'production' ?
+                     'https://tools.wmflabs.org/contraband/'
+                  :  'http://127.0.0.1:8000/';
 
 // method: POST
 export const QueryCreateApi = BASE_API_URI + 'query/add/user/';
 // method: GET, PATCH, DELETE
 export const QueryDetailApi = BASE_API_URI + 'query/<hash>/update/user/';
 // method: POST
-export const filterCreateApi = BASE_API_URI + 'query/<hash>/add/filter/'; 
+export const filterCreateApi = BASE_API_URI + 'query/<hash>/add/filter/';
 // method: GET, PATCH, DELETE
 export const filterDetailApi = BASE_API_URI + 'query/<hash>/update/filter/';
 // method: POST
