64c6580665ac8105e94ae931d597d5d3899cbe0d
Muskan <muskangupta1812@gmail.com>
2020-03-24 18:11:58 +0530

    [feat]: hash_code field added in readonly_fields implemented and enhanced query model UI

--- backend/WikiContrib/query/admin.py
+++ backend/WikiContrib/query/admin.py
@@ -1,6 +1,27 @@
-from WikiContrib.admin import admin_site as admin
+from django.contrib import admin
+from WikiContrib.admin import admin_site
 from .models import Query, QueryFilter, QueryUser
 
-admin.register(Query)
-admin.register(QueryFilter)
-admin.register(QueryUser)
+class QueryAdmin(admin.ModelAdmin):  
+    """
+    Customize the presentation of the model Query
+    """
+
+    fields = (
+        'hash_code', 
+        'file', 
+        'csv_file', 
+        'created_on'
+    )
+    list_display = (
+        'hash_code',
+        'file',
+        'created_on'
+    )
+    readonly_fields = [
+        'hash_code'
+    ]
+
+admin_site.register(Query, QueryAdmin)
+admin_site.register(QueryFilter)
+admin_site.register(QueryUser)
