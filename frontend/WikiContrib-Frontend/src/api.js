5cc768030911a2897f45234db5cf2c6b762afb8e
Raymond Ndibe <ndiberaymond1@gmail.com>
2020-05-18 22:21:53 +0100

    changed tool urls to reflect the changes made to toolforge root url

--- frontend/WikiContrib-Frontend/src/api.js
+++ frontend/WikiContrib-Frontend/src/api.js
@@ -3,7 +3,7 @@
 */
 
 const BASE_API_URI = process.env.NODE_ENV === 'production' ?
-                     'https://tools.wmflabs.org/contraband/'
+                     'https://contraband.toolforge.org'
                   :  'http://127.0.0.1:8000/';
 
 // method: POST
