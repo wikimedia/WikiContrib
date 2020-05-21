e59f1d7d88ca5809ca374237c8ac90c9e445df75
Ndibe Raymond Olisaemeka <NdibeRaymond1@gmail.com>
2020-05-21 02:32:32 +0100

    changed tool urls to reflect the changes made to toolforge root url (#192)

--- backend/WikiContrib/WikiContrib/settings.py
+++ backend/WikiContrib/WikiContrib/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tools.wmflabs.org']
+ALLOWED_HOSTS = ['wikicontrib.toolforge.org']
 
 BASE_URL = env('BASE_URL_REMOTE')
 
